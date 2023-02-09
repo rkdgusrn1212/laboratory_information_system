1443f40ae78580798acbb2cba0ae8fc8f41ba5e0
rkdgusrn1212 <khgkjg12@naver.com>
2023-02-10 00:56:40 +0900

    stepRrn 레이아웃 완성

--- front/src/components/receptconsultation/StepRrn.tsx
+++ front/src/components/receptconsultation/StepRrn.tsx
@@ -8,21 +8,33 @@ const StepRrn: React.FC = () => {
   const rrnRef = useRef(null);
 
   return (
-    <Box>
-      <Typography my={2} variant="h5">
-        접수를 위해 주민번호 13자리를 입력해 주세요.
-      </Typography>
-      <Box display="flex" alignItems="center" justifyContent="center" gap={1}>
-        <RrnMaskedInput
-          fullWidth
-          label="주민번호/외국인등록번호"
-          ref={rrnRef}
-          size="small"
-        />
-        <Button sx={{ height: '100%' }} variant="contained" color="secondary">
-          입력
-        </Button>
+    <Box display="flex" py={6} justifyContent={'space-between'} gap={3}>
+      <Box flexGrow={1} ml={1} maxWidth={500}>
+        <Typography my={2} variant="h5">
+          접수를 위해 주민번호 13자리를 입력해 주세요.
+        </Typography>
+        <Box display="flex" alignItems="center" justifyContent="center" gap={1}>
+          <RrnMaskedInput
+            fullWidth
+            label="주민번호/외국인등록번호"
+            ref={rrnRef}
+            size="small"
+          />
+          <Button sx={{ height: '100%' }} variant="contained" color="secondary">
+            입력
+          </Button>
+        </Box>
       </Box>
+      <Box
+        component="img"
+        mr={{ xs: 1, md: 5 }}
+        sx={{
+          maxHeight: 167,
+          maxWidth: 250,
+        }}
+        alt="The house from the offer."
+        src="/images/idcard.png"
+      />
     </Box>
   );
 };
