78e32fe82b3bf8d079225e511e88d0dd630039dd
류호진 <rhj1216@naver.com>
2023-02-12 19:01:58 +0900

    코드 eslint와 프리티어 적용

--- front/src/components/inadequate/StaffList.js
+++ front/src/components/inadequate/StaffList.js
@@ -1,22 +1,20 @@
-
-import axios from "axios";
+import axios from 'axios';
 
 export const staffList = async () => {
   try {
-    const response = await axios.get("http://localhost:8080/api/collect/staffall");
+    const response = await axios.get(
+      'http://localhost:8080/api/collect/staffall',
+    );
 
     //전처리
     response.data.map((staff) => {
-      if (staff.staffType === 1)
-        staff.staffType = "의사"
+      if (staff.staffType === 1) staff.staffType = '의사';
       else {
-        staff.staffType = "간호사"
+        staff.staffType = '간호사';
       }
-    }
-    )
+    });
 
     return response.data;
-
   } catch (error) {
     console.log(error);
   }
