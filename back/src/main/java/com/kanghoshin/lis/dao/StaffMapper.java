b64b74e3ca2058c9eda260a5f66c3f83d77d217c
rkdgusrn1212 <khgkjg12@naver.com>
2023-02-14 16:27:57 +0900

    백엔드에 Read Staff List API 추가
    - 기존 select 문들에서 non string 자료형에대한 검색방식 개선, CAST 함수를 활용한 정수형 자료의 부분일치검색을 구현, 한자리 미만의 도메인을 가진 정수 또는 boolean 컬럼에 대해선 = 연산으로 제한

--- back/src/main/java/com/kanghoshin/lis/dao/StaffMapper.java
+++ back/src/main/java/com/kanghoshin/lis/dao/StaffMapper.java
@@ -7,6 +7,7 @@ import org.apache.ibatis.annotations.Options;
 import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
 import com.kanghoshin.lis.dto.auth.DetailsDto;
+import com.kanghoshin.lis.dto.staff.ReadStaffListDto;
 import com.kanghoshin.lis.vo.entity.StaffVo;
 
 @Mapper
@@ -30,4 +31,72 @@ public interface StaffMapper {
 			+ " #{detailsDto.staffRrn})")
 	@Options(useGeneratedKeys=true,  keyProperty="detailsDto.staffNo")
 	int insertDetailsDto(@Param("detailsDto") DetailsDto detailsDto);
+	
+
+	@Select("<script>"
+			+ "SELECT staff_no as staffNo"
+			+ ", staff_name as staffName"
+			+ ", staff_male as staffMale"
+			+ ", staff_phone as staffPhone"
+			+ ", staff_rrn as staffRrn"
+			+ ", staff_birth as staffBirth"
+			+ " FROM staff "
+			+ "<where> "
+			+ "<if test='readStaffListDto.staffAdmittedKey != null'> "
+			+ "staff_admitted = #{readStaffListDto.staffAdmittedKey} "
+			+ "</if> "
+			+ "<if test='readStaffListDto.staffTypeKey != null'> "
+			+ "AND staff_type = #{readStaffListDto.staffTypeKey} "
+			+ "</if> "
+			+ "<if test='readStaffListDto.staffNoKey != null'> "
+			+ "AND staff_no like CONCAT('%',#{readStaffListDto.staffNoKey},'%') "
+			+ "</if> "
+			+ "<if test='readStaffListDto.staffNameKey != null'> "
+			+ "AND staff_name like CONCAT('%',#{readStaffListDto.staffNameKey},'%') "
+			+ "</if> "	
+			+ "<if test='readStaffListDto.staffMaleKey != null'> "
+			+ "AND staff_male like CONCAT('%',#{readStaffListDto.staffMaleKey},'%') "
+			+ "</if> "
+			+ "<if test='readStaffListDto.staffPhoneKey != null'> "
+			+ "AND staff_phone like CONCAT('%',#{readStaffListDto.staffPhoneKey},'%') "
+			+ "</if> "
+			+ "<if test='readStaffListDto.staffRrnKey != null'> "
+			+ "AND staff_rrn like CONCAT('%',#{readStaffListDto.staffRrnKey},'%') "
+			+ "</if> "
+			+ "<if test='readStaffListDto.staffBirthStart != null'> "
+			+ "AND staff_birth &gt;= #{readStaffListDto.staffBirthStart} "
+			+ "</if> "
+			+ "<if test='readStaffListDto.staffBirthEnd != null'> "
+			+ "AND staff_birth &lt;= #{readStaffListDto.staffBirthEnd} "
+			+ "</if> "
+			+ "</where> "
+			+ "<trim prefix='ORDER BY' prefixOverrides=','> "
+			+ "<if test='readStaffListDto.staffAdmittedOrder != null'> "
+			+ " staff_admitted ${readStaffListDto.staffAdmittedOrder} "
+			+ "</if> "
+			+ "<if test='readStaffListDto.staffTypeOrder != null'> "
+			+ ", staff_type ${readStaffListDto.staffTypeOrder} "
+			+ "</if> "
+			+ "<if test='readStaffListDto.staffMaleOrder != null'> "
+			+ ", staff_male ${readStaffListDto.staffMaleOrder} "
+			+ "</if> "
+			+ "<if test='readStaffListDto.staffNameOrder != null'> "
+			+ ", staff_name ${readStaffListDto.staffNameOrder} "
+			+ "</if> "
+			+ "<if test='readStaffListDto.staffPhoneOrder != null'> "
+			+ ", staff_phone ${readStaffListDto.staffPhoneOrder} "
+			+ "</if> "
+			+ "<if test='readStaffListDto.staffRrnOrder != null'> "
+			+ ", staff_rrn ${readStaffListDto.staffRrnOrder} "
+			+ "</if> "
+			+ "<if test='readStaffListDto.staffBirthOrder != null'> "
+			+ ", staff_birth ${readStaffListDto.staffBirthOrder} "
+			+ "</if> "
+			+ "<if test='readStaffListDto.staffNoOrder != null'> "
+			+ ", staff_no ${readStaffListDto.staffNoOrder} "
+			+ "</if> "
+			+ "</trim> "
+			+ "limit #{readStaffListDto.pageStart}, #{readStaffListDto.pageSize}"
+			+ "</script>")
+	StaffVo[] select(@Param("readStaffListDto") ReadStaffListDto readStaffListDto);
 }
