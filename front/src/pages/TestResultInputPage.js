26518f91d1131784e268c3795ccb7f7f3bc4349c
shinndrome <kevin91503918@gmail.com>
2023-02-20 16:22:55 +0900

    동신추가

--- front/src/pages/TestResultInputPage.js
+++ front/src/pages/TestResultInputPage.js
@@ -4,16 +4,14 @@ import CssBaseline from '@mui/material/CssBaseline';
 import Grid from '@mui/material/Grid';
 import Paper from '@mui/material/Paper';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
-import TextField from '@mui/material/TextField';
-import InputDialog from '../components/testresultinput/InputDialog';
 import ProgressChart from '../components/testresultinput/ProgressChart';
-import ResultList from '../components/testresultinput/ResultList';
+import { InputDialog, ResultList } from '../components/testresultinput/ResultList';
 
 const lightTheme = createTheme({ palette: { mode: 'light' } });
 
 export default function TestResultInputPage() {
-  return (
 
+  return (
     <ThemeProvider theme={lightTheme}>
       <CssBaseline />
       <Grid container spacing={2}>
@@ -28,15 +26,9 @@ export default function TestResultInputPage() {
             }}
           >
             <Paper elevation={6}>
-              <Typography>바코드 수기 입력</Typography>
-              <TextField
-                sx={{
-                  width: 600,
-                  maxWidth: '80%',
-                }}
-                id="outlined-size-small"
-                size="small"
-              />
+              <Grid sx={{ mx: 2, my: 1 }}>
+                <Typography>바코드 수기 입력</Typography>
+              </Grid>
               <Grid >
                 <InputDialog></InputDialog>
               </Grid>
