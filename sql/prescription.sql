fa82f9c93ca0587546babb643ad9ce3ba4828e7f
rkdgusrn1212 <khgkjg12@naver.com>
2023-02-22 17:14:26 +0900

    test_prescription, prescription db 수정, 더미 데이터 추가

--- sql/prescription.sql
+++ sql/prescription.sql
@@ -32,14 +32,62 @@ CREATE TABLE IF NOT EXISTS `prescription` (
   CONSTRAINT `FK_prescription_prescription_classification` FOREIGN KEY (`prescription_classification_code`) REFERENCES `prescription_classification` (`prescription_classification_code`) ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
--- 테이블 데이터 kanghoshin_lis.prescription:~2 rows (대략적) 내보내기
+-- 테이블 데이터 kanghoshin_lis.prescription:~40 rows (대략적) 내보내기
 DELETE FROM `prescription`;
 /*!40000 ALTER TABLE `prescription` DISABLE KEYS */;
 INSERT INTO `prescription` (`prescription_code`, `behavior_code`, `prescription_name`, `prescription_classification_code`, `prescription_slip_code`, `prescription_comment`) VALUES
+	('A0082', 'D0001010', 'Ach-r-binding Ab', 'CP', NULL, NULL),
+	('A0237', 'D0001010', 'Anti Cardiolipin IgG', 'CP', NULL, NULL),
 	('A0314', 'D4902010', '비타민검사', 'CP', NULL, NULL),
+	('A0341', 'D0001010', '1CTP', 'CP', NULL, NULL),
+	('A0352', 'D0001010', 'Acetazolamide', 'CP', NULL, NULL),
+	('A0414', 'D0001010', '1,5-Anhydroglucitol', 'CP', NULL, NULL),
+	('A0420', 'D0001010', '2,5-Hexanedione', 'CP', NULL, NULL),
+	('A0504', 'D0001010', 'C. difficile Toxin A/B', 'CP', NULL, NULL),
+	('A0547', 'D0001010', 'Anion Gap', 'CP', NULL, NULL),
+	('A0801', 'D0001010', 'Adenovirus Culture', 'CP', NULL, NULL),
+	('A5165', 'D0001010', 'Antiplasmin', 'CP', NULL, NULL),
+	('A6047', 'D0001010', 'APCR', 'CP', NULL, NULL),
+	('A6065', 'D0001010', 'Anti-Xa LMWH test', 'CP', NULL, NULL),
+	('A6163', 'D0001010', 'C. difficile Culture', 'CP', NULL, NULL),
+	('A6510', 'D0001010', 'Acylcarnitine 정량', 'CP', NULL, NULL),
+	('A6590', 'D0001010', 'AVPR2 gene mutation', 'CP', NULL, NULL),
+	('B0680', 'D0001010', 'Egg count', 'CP', NULL, NULL),
+	('B1070', 'D0001010', 'Eosinophil count', 'CP', NULL, NULL),
+	('B1530', 'D0001010', 'APTT', 'CP', NULL, NULL),
+	('B2023', 'D0001010', 'Du Test', 'CP', NULL, NULL),
+	('B2510', 'D0001010', 'Albumin', 'CP', NULL, NULL),
+	('B2531', 'D0001010', '100g OGTT (임신성 당뇨)', 'CP', NULL, NULL),
+	('B2535', 'D0001010', '50g OGTT (임신성 당뇨 스크리닝)', 'CP', NULL, NULL),
+	('B2580', 'D0001010', 'ALT (SGPT)', 'CP', NULL, NULL),
+	('B2601', 'D0001010', 'Acid phosphatase', 'CP', NULL, NULL),
+	('B3081', 'D0001010', 'Dyshemoglobin Test', 'CP', NULL, NULL),
+	('B4052', 'D0001010', 'AFB Culture', 'CP', NULL, NULL),
+	('B4114', 'D0001010', 'CRE Culture', 'CP', NULL, NULL),
+	('B5290', 'D0001010', 'AIDS(HIV) Ag/Ab (combo)', 'CP', NULL, NULL),
+	('B53611', 'D0001010', 'CMV IgM', 'CP', NULL, NULL),
+	('C3221', 'D0001010', '5-HIAA (Qual)', 'CP', NULL, NULL),
+	('C4691', 'D0001010', 'Adenovirus Ag (Stool)', 'CP', NULL, NULL),
 	('D0001', 'D0001010', '일반혈액검사1', 'CP', NULL, '하나만 요청'),
+	('D083200F', 'D083200F', '골수판독검', 'CP', NULL, '검사필'),
+	('H0225', 'D0001010', 'DMD/BMD Exon Deletion', 'CP', NULL, NULL),
+	('H0576', 'D0001010', 'ABCB11 gene mutation (PFIC2)', 'CP', NULL, NULL),
+	('H0667', 'D0001010', '(1-3)β-D-Glucan', 'CP', NULL, NULL),
+	('H0735', 'D0001010', 'AR gene, mutation (androgen insensitivity)', 'CP', NULL, NULL),
+	('H0827', 'D0001010', 'ALB gene, mutation [sequencing]', 'CP', NULL, NULL),
 	('LB0282', 'D0001010', '기본혈액분석', 'CP', NULL, NULL);
 /*!40000 ALTER TABLE `prescription` ENABLE KEYS */;
+
+-- 트리거 kanghoshin_lis.prescription_after_insert 구조 내보내기
+SET @OLDTMP_SQL_MODE=@@SQL_MODE, SQL_MODE='STRICT_TRANS_TABLES,ERROR_FOR_DIVISION_BY_ZERO,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION';
+DELIMITER //
+CREATE TRIGGER `prescription_after_insert` AFTER INSERT ON `prescription` FOR EACH ROW BEGIN
+IF NEW.prescription_classification_code = 'CP' THEN
+INSERT  test_prescription(prescription_code) VALUES(NEW.prescription_code);
+END IF;
+END//
+DELIMITER ;
+SET SQL_MODE=@OLDTMP_SQL_MODE;
 
 -- 트리거 kanghoshin_lis.prescription_before_insert 구조 내보내기
 SET @OLDTMP_SQL_MODE=@@SQL_MODE, SQL_MODE='STRICT_TRANS_TABLES,ERROR_FOR_DIVISION_BY_ZERO,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION';
@@ -48,7 +96,7 @@ CREATE TRIGGER `prescription_before_insert` BEFORE INSERT ON `prescription` FOR 
 IF NEW.prescription_classification_code != null THEN
 SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'DO NOT INSERT prescription_classification_code VALUE';
 END IF;
-IF LEFT(NEW.behavior_code,1) = 'D' THEN
+IF LEFT(NEW.behavior_code, 1) = 'D' THEN
 SET NEW.prescription_classification_code = 'CP';
 END IF;
 END//
--- sql/test_prescription.sql
+++ sql/test_prescription.sql
@@ -20,24 +20,62 @@ USE `kanghoshin_lis`;
 -- 테이블 kanghoshin_lis.test_prescription 구조 내보내기
 CREATE TABLE IF NOT EXISTS `test_prescription` (
   `prescription_code` varchar(10) NOT NULL,
-  `specimen_type_code` char(2) NOT NULL,
-  `specimen_container_code` char(2) NOT NULL,
-  `test_prescription_reference` varchar(200) DEFAULT NULL,
+  `specimen_type_code` char(2) DEFAULT NULL,
+  `specimen_container_code` char(2) DEFAULT NULL,
+  `test_prescription_amount` int(11) DEFAULT NULL,
   `test_prescription_unit` varchar(10) DEFAULT NULL,
-  `test_field` int(11) NOT NULL,
+  `test_prescription_reference` varchar(200) DEFAULT NULL,
+  `test_field_code` char(4) DEFAULT NULL,
   PRIMARY KEY (`prescription_code`) USING BTREE,
   KEY `FK_test_prescription_specimen_type` (`specimen_type_code`) USING BTREE,
   KEY `FK_test_prescription_specimen_container` (`specimen_container_code`) USING BTREE,
-  KEY `FK_test_prescription_test_field` (`test_field`),
+  KEY `FK_test_prescription_test_field` (`test_field_code`),
   CONSTRAINT `FK_test_prescription_prescription` FOREIGN KEY (`prescription_code`) REFERENCES `prescription` (`prescription_code`) ON DELETE CASCADE ON UPDATE CASCADE,
   CONSTRAINT `FK_test_prescription_specimen_container` FOREIGN KEY (`specimen_container_code`) REFERENCES `specimen_container` (`specimen_container_code`) ON UPDATE CASCADE,
   CONSTRAINT `FK_test_prescription_specimen_type` FOREIGN KEY (`specimen_type_code`) REFERENCES `specimen_type` (`specimen_type_code`) ON UPDATE CASCADE,
-  CONSTRAINT `FK_test_prescription_test_field` FOREIGN KEY (`test_field`) REFERENCES `test_field` (`test_field_no`) ON UPDATE CASCADE
+  CONSTRAINT `FK_test_prescription_test_field` FOREIGN KEY (`test_field_code`) REFERENCES `test_field` (`test_field_code`) ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
--- 테이블 데이터 kanghoshin_lis.test_prescription:~0 rows (대략적) 내보내기
+-- 테이블 데이터 kanghoshin_lis.test_prescription:~36 rows (대략적) 내보내기
 DELETE FROM `test_prescription`;
 /*!40000 ALTER TABLE `test_prescription` DISABLE KEYS */;
+INSERT INTO `test_prescription` (`prescription_code`, `specimen_type_code`, `specimen_container_code`, `test_prescription_amount`, `test_prescription_unit`, `test_prescription_reference`, `test_field_code`) VALUES
+	('A0082', '01', '01', 1, 'ml', '<  0.5  nmol/L', 'DI01'),
+	('A0237', '01', '01', 1, 'ml', 'Negative  ≤  20  CU', 'DI01'),
+	('A0341', '01', '01', 2, 'ml', '≤4.49  ng/mL', 'DI01'),
+	('A0352', '01', '01', 2, 'ml', 'Not  established  ug/mL', 'GI01'),
+	('A0414', '01', '01', 1, 'mL', '≥14.00 ug/mL', 'CC01'),
+	('A0420', '05', '98', 10, 'ml', '< 5.00 mg/L', 'GI01'),
+	('A0504', '22', '10', 2, 'g', 'Negative', 'CM01'),
+	('A0547', '01', '01', 1, 'mL', 'Anion Gap : 10.00~20.00 mEq/L', 'CC01'),
+	('A0801', '05', '98', 5, 'mL', 'No  Adeno  virus  isolated', 'CM01'),
+	('A5165', '03', '03', 1, 'mL', '80  ~  130  %', 'DH01'),
+	('A6047', '03', '03', 1, 'mL', 'Negative', 'DH01'),
+	('A6065', '03', '03', 1, 'mL', '0.3-0.7 IU/mL (헤파린 투여자의 적정치료범위)', 'DH01'),
+	('A6163', '22', '10', 10, 'g', '없음', 'CM01'),
+	('A6510', '02', '02', 3, 'ml', '없음', 'GI01'),
+	('A6590', '02', '02', 3, 'ml', '없음', 'MG01'),
+	('B0680', '22', '10', 10, 'g', '없음', 'CM01'),
+	('B1070', '02', '02', 3, 'mL', '50.0 ~ 500.0/uL', 'DH01'),
+	('B1530', '03', '03', 1, 'mL', '30.5 ~ 45 Sec', 'DH01'),
+	('B2023', '02', '02', 2, 'mL', '없음', 'DH01'),
+	('B2510', '01', '01', 1, 'mL', '3.50  ~  5.20  g/dL', 'CC01'),
+	('B2531', '01', '01', 1, 'mL', '공복: < 95 mg/dL, 1시간: < 180 mg/dL, 2시간: < 155 mg/dL, 3시간: < 140 mg/dL', 'CC01'),
+	('B2535', '01', '01', 1, 'mL', '< 140 mg/dL', 'CC01'),
+	('B2580', '01', '01', 1, 'mL', '≤41 U/L', 'CC01'),
+	('B2601', '01', '01', 1, 'mL', '여자 : ≤6.5, 남자 : ≤6.6 U/L', 'CC01'),
+	('B3081', '02', '02', 2, 'mL', 'Met Hb : < 1.5%, Carboxy Hb(CO-Hb) : 비흡연 ≤ 2.0', 'DH01'),
+	('B4052', '05', '98', 10, 'mL', '없음', 'CM01'),
+	('B4114', '22', '10', 5, 'g', '없음', 'CM01'),
+	('B5290', '01', '01', 1, 'ml', 'Negative', 'DI01'),
+	('B53611', '01', '01', 1, 'ml', 'Non-Reactive < 0.85, Gray Zone 0.85 ~ <  1.0, Reactive ≥ 1.0 Index', 'DI01'),
+	('C3221', '05', '98', 10, 'ml', 'Negative', 'GI01'),
+	('C4691', '22', '10', 5, 'g', 'Negative', 'CM01'),
+	('H0225', '02', '02', 3, 'ml', '없음', 'MG01'),
+	('H0576', '02', '02', 4, 'ml', '없음', 'MG01'),
+	('H0667', '01', '01', 1, 'ml', 'Negative < 60.0, Intermediate 60.0~79.9, Positive ≥ 80.0 pg/mL', 'GI01'),
+	('H0735', '02', '02', 4, 'ml', '없음', 'MG01'),
+	('H0827', '02', '02', 4, 'ml', '없음', 'MG01');
 /*!40000 ALTER TABLE `test_prescription` ENABLE KEYS */;
 
 -- 트리거 kanghoshin_lis.test_prescription_after_delete 구조 내보내기
@@ -45,15 +83,6 @@ SET @OLDTMP_SQL_MODE=@@SQL_MODE, SQL_MODE='STRICT_TRANS_TABLES,ERROR_FOR_DIVISIO
 DELIMITER //
 CREATE TRIGGER `test_prescription_after_delete` AFTER DELETE ON `test_prescription` FOR EACH ROW BEGIN
 UPDATE prescription SET prescription_classification_code = null WHERE prescription_code = OLD.prescription_code;
-END//
-DELIMITER ;
-SET SQL_MODE=@OLDTMP_SQL_MODE;
-
--- 트리거 kanghoshin_lis.test_prescription_before_insert 구조 내보내기
-SET @OLDTMP_SQL_MODE=@@SQL_MODE, SQL_MODE='STRICT_TRANS_TABLES,ERROR_FOR_DIVISION_BY_ZERO,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION';
-DELIMITER //
-CREATE TRIGGER `test_prescription_before_insert` BEFORE INSERT ON `test_prescription` FOR EACH ROW BEGIN
-UPDATE prescription SET prescription_classification_code = 'CP' WHERE prescription_code = NEW.prescription_code;
 END//
 DELIMITER ;
 SET SQL_MODE=@OLDTMP_SQL_MODE;
